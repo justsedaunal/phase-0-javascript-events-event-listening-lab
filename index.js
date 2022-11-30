0cbc5dbb169799cfda091d2af4360a72e3a8f16e
Seda Ünal <sedaylmz06@outlook.com>
2022-12-01 00:49:17 +0300

    addeventlistener

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+  console.log("adding event listener");
 }
+
+const input = document.getElementById("button");
+input.addEventListener("click", function () {
+  alert("I was clicked!");
+});
